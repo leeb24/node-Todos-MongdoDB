c80551018a0edb1e9b1146ba2021ac749d54d457
ByoungSul lee <byoungsullee@dhcp-wifi-8021x-155-41-97-35.bu.edu>
2018-08-15 17:46:07 -0400

    login

--- Server/server.js
+++ Server/server.js
@@ -114,7 +114,9 @@ app.post('/users/login',(req,res)=>{
     var body = _.pick(req.body, ['email', 'password'])
     
     User.findByCredentials(body.email,body.password).then((user)=>{
-        res.send(user);
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth', token).send(user);
+        })
     }).catch((e)=>{
         res.status(400).send();
     });
