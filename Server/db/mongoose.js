dc87e4609a8d55fd20a104a7e835feb6cf6e51e3
ByoungSul lee <byoungsullee@dhcp-wifi-8021x-155-41-97-35.bu.edu>
2018-08-15 12:50:31 -0400

    mlab

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 // mongodb://test:dlqudtjf24@ds119422.mlab.com:19422/test-todo-db
 //DB 
 module.exports = {
--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ var { User } = require('./models/User')
 const { ObjectID } = require('mongodb')
 
 var app = express()
-const port = 3000 // process.env.PORT
+const port = process.env.PORT; // process.env.PORT
 app.use(bodyParser.json())
 app.post('/todos', (req, res) => {
   var todo = new Todo({
