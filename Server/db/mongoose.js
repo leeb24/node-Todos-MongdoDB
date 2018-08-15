767728c68f9a01e52bf7d12729c06d57fad63fd3
ByoungSul lee <byoungsullee@dhcp-wifi-8021x-155-41-97-35.bu.edu>
2018-08-15 12:02:34 -0400

    Auth

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost:27017/TodoApp')
 // mongodb://test:dlqudtjf24@ds119422.mlab.com:19422/test-todo-db
+//DB 
 module.exports = {
   mongoose: mongoose
 }
