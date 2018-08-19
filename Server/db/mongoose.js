17a2ca36e793f73477331d9f6d7db53793f22b0f
ByoungSul lee <byoungsullee@ByoungSului-MacBook-Pro.local>
2018-08-19 10:47:06 -0400

    JWT

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect(mongodb://test:dlqudtjf24@ds121332.mlab.com:21332/heroku_ttk4gzzx || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://test:dlqudtjf24@ds121332.mlab.com:21332/heroku_ttk4gzzx' || 'mongodb://localhost:27017/TodoApp`)
 
 // mongodb://test:dlqudtjf24@ds119422.mlab.com:19422/test-todo-db
 //mongodb://heroku_ttk4gzzx:3vujp9i4ksoh58g2mnlnmk7ghi@ds121332.mlab.com:21332/heroku_ttk4gzzx
