bfd82dc7edb919c8a346d47c2fb990015217bdb8
leeb24 <leeb@bu.edu>
2019-07-18 17:01:35 -0400

    Update mongoose.js

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -1,12 +1,8 @@
 var mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://test2:dlqudtjf24@ds121332.mlab.com:21332/heroku_ttk4gzzx')
+mongoose.connect('mongodb://test2:????.mlab.com:21332/heroku_ttk4gzzx')
 
-// mongodb://test:dlqudtjf24@ds119422.mlab.com:19422/test-todo-db
-//mongodb://heroku_ttk4gzzx:3vujp9i4ksoh58g2mnlnmk7ghi@ds121332.mlab.com:21332/heroku_ttk4gzzx
-//DB 
-//'mongodb://localhost:27017/TodoApp
-//mongodb://test:dlqudtjf24@ds121332.mlab.com:21332/heroku_ttk4gzzx
+
 
 module.exports = {
   mongoose: mongoose
